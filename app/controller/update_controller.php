96ba95ed89b2787ebd4977b266c578ef764c8e99
unknown <acacio.junio28@gmail.com>
2021-11-06 17:01:25 -0300

    sincronizar versão

--- app/controller/update_controller.php
+++ app/controller/update_controller.php
@@ -9,9 +9,9 @@ public function pesquisa($id){
     return $inserir;
     
     }
-public function update($matricula,$nome,$cpf,$tipo,$beneficiario,$idade,$adesao,$desconto,$total,$id){
+public function update($matricula,$nome,$cpf,$tipo,$beneficiario,$idade,$adesao,$desconto,$total,$nascimento,$id){
     $editar=new banco();
-    $editar->update($matricula,$nome,$cpf,$tipo,$beneficiario,$idade,$adesao,$desconto,$total,$id);
+    $editar->update($matricula,$nome,$cpf,$tipo,$beneficiario,$idade,$adesao,$desconto,$total,$nascimento,$id);
     
     }
 }
@@ -19,9 +19,9 @@ $o=new update_controller();
 $id = filter_input ( INPUT_GET ,'id',FILTER_SANITIZE_NUMBER_INT);
 $o->pesquisa($id);
 if ( isset( $_POST['editar'])) {
-    $o-> update ($_POST['matricula'],$_POST['nome'],$_POST['cpf'],$_POST['tipo'],$_POST['beneficiario'],$_POST['idade'],$_POST ['adesao'],$_POST['desconto'],$_POST['total'],$_POST['id']);
+    $o-> update ( $_POST['matricula'],$_POST['nome'],$_POST['cpf'],$_POST['tipo'],$_POST['beneficiario'],$_POST['idade'],$_POST ['adesao'],$_POST['desconto'],$_POST['total'],$_POST['data_nascimento'],$_POST['id']);
     echo  "<script> alert ('Registro incluído com sucesso!');</script>" ;
-    return header("Location:/astaj_valida/listar");
+    return header('Location: /astaj_valida/listar');
    
 }
 
--- public/js/confirm_delete.js
+++ public/js/confirm_delete.js
@@ -14,5 +14,16 @@
             location.reload();
         }
     };
- 
-   
+    
+
+    $(document).ready(function(){
+    $("#search").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
+    /**$(document).ready(function(){
+        alert('teste');
+    });**/
