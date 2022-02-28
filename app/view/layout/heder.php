bb22fb35b19a2ea2e4a4be1021f06b7d7ae5f58b
Carlos Araujo de Sousa <carlos.araujo@ish.com.br>
2022-02-28 12:23:38 -0300

    Sincronizar.

---
+++ app/view/layout/heder.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AEEBC</title>
+    <script src="./public/js/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <script src="https://kit.fontawesome.com/335f4c7504.js" crossorigin="anonymous"></script>
+    <link rel="icon" type="imagem/png" href="public/img/incon.png" \>
+    <link rel="stylesheet" type="text/css" href="./public/css/style.css" media="screen" />
+    <link href="./public/css/bootstrap/bootstrap.css" rel="stylesheet" />
+    <script src="./public/js/bootstrap/bootstrap.js" crossorigin="anonymous"></script>
+    <script src="./public/js/confirm_delete.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg  navbar-light bg-white shadow p-3 mb-5" id="nav">
+<a class="navbar-brand" href="#">
+<img src="public/img/imagem.jfif" width="170" height="70" alt="">
+</a>
+  <div class="cotainer" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/astaj_valida/listar">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/astaj_valida/relatorio">Relatório</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/astaj_valida/configuracao">Configuração</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/astaj_valida/import-csv">Importar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="/astaj_valida/faixaetaria">Configuração</a>
+      </li>
+      <div class="row justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link text-dark " href="app/controller/sair.php">
+        <img src="public/img/sair.png" width="30" height="30" alt="Sair" >
+        </a>
+      </li>
+     </div>
+    </ul>
+  </div>
+</nav>
+
+    
