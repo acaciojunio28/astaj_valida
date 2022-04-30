6c988a83d28c7750dbab2474a8d11fd8d53b99d7
Carlos Araujo de Sousa <carlos.araujo@ish.com.br>
2022-04-30 12:01:54 -0300

    Sincronizar.

--- app/view/alerta.php
+++ app/view/alerta.php
@@ -3,3 +3,4 @@
 <div class="alert alert-danger" role="alert">
 <h3> <i class="fas fa-exclamation-triangle"></i>Você não tem permissão para acessar essa página, procure seu Administrador!</h3>
 </div>
+
